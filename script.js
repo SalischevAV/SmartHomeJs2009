9ae5d62074a33dcea3cf871be71c438e966d61aa
salischevAV <devorer77@gmail.com>
2020-04-25 20:17:26 +0300

    add Regulator & Reciver

--- script.js
+++ script.js
@@ -1,5 +1,67 @@
 "use strict"
 
+const TVCHANNELS = new Map([[1, "UT-1"], [2, "TRK Ukraine"], [3, "1+1"], [4, "ICTV"], [5, "Noviy"], [6, "TET"], [7, "NLO"], [8, "M1"], [9, "Football-1"], [10, "TV1000"]]);
+const RADIOCHANNELS = ["HitFM", "RadioRocks", "Shanson", "RusskoeRadio", "JamFM", "SharmankaFM", "Dovira", "MelodiyaFM"];
+//---------------------------------------------------------------
+function ChannelReciver(channelList = new Map(), currentChannel=[0, ""]){
+    this.__channelList = channelList;
+    this.__currentChannel = currentChannel;
+
+    
+    this.__psevdoChannelList = this.__getArrFromMapKeys();
+    this.__psevdoCurrentCnannel = this.__psevdoChannelList[0];
+    
+}
+
+//channelReciver.prototype.nextChannel = function(){}
+//previousChannel
+//setChannel
+//getCurrentChannel
+ChannelReciver.prototype.__getArrFromMapKeys = function(){
+    let res = [];
+    for (let key of this.__channelList.keys()){
+        res.push(key);
+    }
+    return res;
+}
+
+ChannelReciver.prototype.channelUp = function(){
+    if (this.__psevdoCurrentCnannel >= this.__psevdoChannelList.length){}
+    else (this.__psevdoCurrentCnannel++);
+}
+
+ChannelReciver.prototype.channelDown = function(){
+    if (this.__psevdoCurrentCnannel <= 1){}
+    else (this.__psevdoCurrentCnannel--);
+}
+
+ChannelReciver.prototype.setChannel = function(value){
+    if (typeof value == `number` && value >= 0 && value <= (this.__psevdoChannelList.length-1)){
+        this.__psevdoCurrentCnannel = value;
+    }
+}
+
+/*ChannelReciver.prototype.__returnRealValueChannel = function(){
+    this.__currentChannel[0] = this.__psevdoCurrentCnannel;
+    this.__currentChannel[0] = this.__channelList.get(this.__psevdoCurrentCnannel);
+}*/
+
+ChannelReciver.prototype.getChannel = function(){
+    this.__currentChannel[0] = this.__psevdoCurrentCnannel;
+    this.__currentChannel[1] = this.__channelList.get(this.__psevdoCurrentCnannel);
+    return this.__currentChannel;
+}
+
+
+ChannelReciver.prototype.getChannelList = function(){
+    let res = "";
+    for (let entry of this.__channelList){
+        res += entry + `\n`;
+    }
+    return res;
+}
+
+//-----------------------------------------------------------------
 function Regulator (min =0, max = 100, currentValue = 0){
     this.__min = min;
     this.__max = max;
@@ -24,7 +86,7 @@ Regulator.prototype.currentValueDown = function(){
 }
 
 Regulator.prototype.setCurrentValue = function(value){
-    if (value <= this.__max && value >= this.__min){
+    if (typeof value == `number` && value <= this.__max && value >= this.__min){
         this.__currentValue = value;
     }
 }
@@ -95,11 +157,16 @@ SoundDevice.prototype.setVolume = function(value){
     this._volumeLevel.setCurrentValue(value);
 }
 
+SoundDevice.prototype.getVolume = function(){
+    return this._volumeLevel.getCurrentValue();
+}
+
+
     
-    //--------------------------------------------------------------
+//--------------------------------------------------------------
 
     function Mp3Player(brand, diskName, power = false, volumeLevel = new Regulator(), disk = ["track1", "track2", "track3", "track4", "track5"], currentTrack = 1) {        
-        SoundDevice.call(this, brand, power = false, volumeLevel = new Regulator());
+        SoundDevice.call(this, brand, power = false, volumeLevel = new Regulator() );
         this._currentTrack = currentTrack;
         this._diskName = diskName;
         this._disk = disk;
@@ -132,3 +199,30 @@ SoundDevice.prototype.setVolume = function(value){
         Mp3Player.prototype.toString = function(){
             return SoundDevice.prototype.toString.call(this) + `, disk name: ${this._diskName}, current track: ${this._currentTrack}`;
         }
+
+//------------------------------------------------------------------------
+
+function TV(brand, power = false, volumeLevel = new Regulator(), currentChannel = new ChannelReciver(TVCHANNELS)){
+    SoundDevice.call(this, brand, power = false, volumeLevel = new Regulator() );
+    this._currentChannel = currentChannel;
+}
+
+TV.prototype = Object.create(SoundDevice.prototype);
+TV.prototype.constructor = TV;
+
+TV.prototype.nextChannel= function(){
+    this._currentChannel.channelUp();
+}
+
+TV.prototype.previousChannel = function(){
+    this._currentChannel.channelDown();
+}
+
+TV.prototype.setChannel = function(value){
+    this._currentChannel.setChannel(value);
+}
+
+
+TV.prototype.toString = function(){
+    return SoundDevice.prototype.toString.call(this) + `, current channel: ${this._currentChannel.getChannel()}`
+}
